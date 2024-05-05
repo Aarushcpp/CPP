4b2d2850b6de60a985a30a16792971815c0604bc
Titan-11 <160115504+Aarushcpp@users.noreply.github.com>
2024-05-05 20:19:22 +0530

    Create Q3.cpp

---
+++ PW_Skills/Assignments/Week 1 C++ Assignment 2/Assignment_2/Q3.cpp
@@ -0,0 +1,12 @@
+2
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int x,y;
+// cin>>x>>y;
+// x+=y;// x = 5 y = 3
+// x-=y; x = 2 y = 3
+// x%=y; x = 2 y = 3
+// cout<<x;
+// }
